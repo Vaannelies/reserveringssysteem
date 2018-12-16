692e95a3939dca976828b183885083d30ac608ee
Annelies <43701941+Vaannelies@users.noreply.github.com>
2018-12-16 16:59:39 +0100

    Behandeling kiezen
    Dit is pagina 2. Hierna komt de 'hoofdpagina' waar alle overige informatie wordt gegeven. Je kunt hier voor elke persoon kiezen welke behandeling hij wil. De info van pagina 1 en 2 wordt doorgegeven aan de hoofdpagina. Dus hier wordt nog niks doorgegeven aan de database.

---
+++ behandelingen.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: annel
+ * Date: 10-12-2018
+ * Time: 15:04
+ */
+$aantalpersonen = $_POST['aantal']
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reserveren bij Christa</title>
+</head>
+
+<body>
+
+    <form action="indexpagina.php" method="post">
+        Kies uw behandeling:<br>
+
+        <?php
+        for($i = 0; $i < $aantalpersonen; $i++){ ?>
+        <p>Persoon <?= $i ?></p>
+        <select name="<?= "behandeling".$i ?>" size="10">
+            <option value="knippen">Knippen</option>
+            <option value="verven">Verven</option>
+            <option value="knippenverven">Verven en knippen</option>
+            <option value="kaal">Kaal scheren</option>
+        </select>
+
+        <?php
+        }
+
+        ?>
+        <input type="hidden" name="aantal" value="<?= $aantalpersonen ?>">
+
+        <input type="submit" name="bevestigd" value="Bevestigen">
+
+    </form>
+
+
+
+</body>
+
+</html>
+
+<!--
+​$("#datepicker").datepicker({
+beforeShowDay: function(date) {
+var day = date.getDay();
+return [(day != 1 && day != 2)];
+}
+})​​​​​;​
+
+-->
