0918de5dae5a909b38f18ee34e88ed5ae8f4f42a
Annelies <43701941+Vaannelies@users.noreply.github.com>
2018-12-23 16:54:48 +0100

    Een start pagina met een knop 'Reserveren'. Een soort home-page

---
+++ Start.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: annel
+ * Date: 23-12-2018
+ * Time: 13:46
+ */
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <div class="titel" style="border-style:solid; border-color: lightgrey; width:100vw; background-color: gray;">
+        <h1 style="color:white; margin-left:50px; font-family: arial;">
+            Christa
+        </h1>
+    </div>
+
+    <button onclick="window.location.href = 'aantal personen.php';" style="border-radius:5px; width:300px; height: 200px; font-size: 35px; margin-top: 300px; margin-left: 20%;"> Reserveren </button>
+
+</body>
+</html>
