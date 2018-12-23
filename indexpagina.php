698880883dae6789800cdc0a859e7fe3f5bfc561
Annelies <43701941+Vaannelies@users.noreply.github.com>
2018-12-23 15:54:20 +0100

    Infoknop achter elk veldje bij indexpagina. Infotext geschreven. Ronde hoekjes textveldjes op alle pagina's

--- indexpagina.php
+++ indexpagina.php
@@ -36,6 +36,7 @@ $verven = $_POST['behandeling2'];
 
     <style>
         <?php include 'stylesheet_info.css'; ?>
+        <?php include 'stylesheet_forms.css'; ?>
     </style>
 
 </head>
@@ -44,19 +45,40 @@ $verven = $_POST['behandeling2'];
 
     <form action="bevestigd.php" method="post">
         Voornaam:<br>
-        <input type="text" name="voornaam" maxlength="50" required>
+        <input class="text-box" type="text" name="voornaam" maxlength="50"" required>
             <div class="tooltip">i
-            <span class="tooltiptext">Dit is nodig om...</span>
+            <span class="tooltiptext">Dit is nodig om te weten wie u bent.</span>
             </div>
-<br>
+            <br>
+
         Achternaam:<br>
-        <input type="text" name="achternaam" maxlength="100" required><br>
+        <input class="text-box" type="text" name="achternaam" maxlength="100" required>
+            <div class="tooltip">i
+            <span class="tooltiptext">Dit is nodig om te weten wie u bent.</span>
+            </div>
+            <br>
+
         Datum:<br>
-        <input type="date" name="datum" required><br>
+        <input class="text-box" type="date" name="datum" required>
+          <div class="tooltip">i
+          <span class="tooltiptext">Dit is nodig om te weten op welke dag er gereserveerd moet worden.</span>
+          </div>
+          <br>
+
         Tijd:<br>
-        <input type="time" name="tijd" min="18:30" max="20:30" required>  Kies een tijd tussen 18:30 uur en 20:30 uur.<br>
+        <input class="text-box" type="time" name="tijd" min="18:30" max="20:30" required>  Kies een tijd tussen 18:30 uur en 20:30 uur.
+          <div class="tooltip">i
+          <span class="tooltiptext">Dit is nodig om te weten om welke tijd er gereserveerd moet worden.</span>
+          </div>
+          <br>
+
         Telefoonnummer: <br>
-        <input type="text" name="telefoon" maxlength="20" required><br>
+        <input class="text-box" type="text" name="telefoon" maxlength="20" required>
+          <div class="tooltip">i
+          <span class="tooltiptext">Dit is nodig om u te kunnen bereiken als dat nodig is.</span>
+          </div>
+          <br>
+
     <!--    Aantal personen: <br> -->
         <input type="hidden" name="aantal" min="1" max="10" value="<?= $aantalpersonen ?>" required><br>
         <input type="hidden" name="behandeling1" value="<?= $knippen ?>"<br>
