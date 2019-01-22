3112668002871180e2bd4f4b99a61858bfbf6bff
Annelies <43701941+Vaannelies@users.noreply.github.com>
2019-01-22 23:57:02 +0100

    Betere opmaak

--- details.php
+++ details.php
@@ -36,17 +36,92 @@ if (!isset($_SESSION['username'])){
     $city = $row['city'];
 
 
+
     ?>
 
-    <h1>Reservering op <?= $date ?> om <?= $time ?> uur.</h1>
-    <p><?= $firstname ?> <?= $lastname ?><br><br>
-        Telefoonnummer:<?= $phone ?><br>
-        Adres: <?= $street ?> <?= $housenumber ?><br>
-        Plaats: <?= $city ?></p>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <link rel="stylesheet" href="stylesheet_home.css">
+    </head>
+    <body>
 
-    Aantal personen: <?= $amount ?><br>
-    Aantal mensen knippen: <?= $behandeling1 ?><br>
-    Aantal mensen verven: <?= $behandeling2 ?><br>
+
+    <header>
+
+        <div class="titel">
+            <h1 style="color:white; margin-left:50px; font-family: arial;">
+                <a href="Start.php" style="color:white; text-decoration-line:none;">Christa</a>
+            </h1>
+        </div>
+        <div class="ondertitel">
+            <h1 style="color:white; margin-left:50px; font-family: arial; font-size:20px;">
+                Details
+            </h1>
+            <h1 style="display: flex; justify-content: flex-end; color:white; margin-left:50px; font-family: arial; font-size:20px; margin-top:-38px; padding-right:10px;">
+
+
+                <!--  <div style="margin-left:60%;"> -->
+                <button onclick="goBack()" style="border-radius:5px;
+                    width:100px; height: 30px;
+                     font-size: 16px;">Terug</button>
+                <button onclick="window.location.href = 'logout.php';"
+                        style="border-radius:5px;
+                    width:100px; height: 30px;
+                     font-size: 16px;">
+                    Log uit</button>
+                <!-- </div> -->
+            </h1>
+
+        </div>
+        <div style="margin-left:50px;">
+
+            <h1>Reservering van <?= $firstname ?> <?= $lastname ?></h1>
+        </div>
+    </header>
+
+
+    <div style="margin-top: 100px; display:flex; justify-content:space-evenly;">
+        <div> <!-- Personal stuff -->
+            <h2>Persoonlijke gegevens</h2>
+            <p>Naam: <?= $firstname ?> <?= $lastname ?><br><br>
+            Telefoonnummer: <?= $phone ?><br><br>
+            Adres: <?= $street ?> <?= $housenumber ?><br><br>
+            Plaats: <?= $city ?>
+            </p>
+<br><br><br>
+            <h2>Wanneer</h2>
+            <p style="font-size: 30px;"><?= $date ?> om <?= $time ?> uur.</p>
+
+        </div>
+
+        <div
+        style="border: solid 1px black; padding: 10px; padding-right: 15px;"> <!-- Reservation stuff -->
+            <h2 style="text-decoration-line: underline;">To do</h2>
+            <p style="text-decoration-line: underline;">  Aantal personen: <?= $amount ?><br><br></p>
+            <p style="border-bottom: 1px black solid;"> Van al deze mensen moet u er </p>
+            <p style="border-bottom: 1px black solid;">    <?= $behandeling1 ?> knippen</p>
+            <p style="border-bottom: 1px black solid;"> <?= $behandeling2 ?> verven<br>
+            </p>
+        </div>
+    </div>
+
+    </body>
+
+    <script>
+        function goBack() {
+            window.history.back();
+
+        }
+    </script>
+
+    </html>
+
 
     <?php
 
