376cf48aacaff0f47b4b6e74f945458872f97f2b
Annelies <43701941+Vaannelies@users.noreply.github.com>
2019-01-22 21:43:18 +0100

    pagina opgemaakt

--- verwijderen.php
+++ verwijderen.php
@@ -25,23 +25,74 @@ $row = $results->fetch_assoc();
 
 
 ?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <link rel="stylesheet" href="stylesheet_home.css">
+    </head>
+    <body>
 
-Weet u zeker dat u de reservering van <?=$row['firstname']?> <?=$row['lastname']?> wilt verwijderen?
+    <header>
 
-<script>
-    function goBack() {
-        window.history.back();
-    }
-</script>
+        <div class="titel">
+            <h1 style="color:white; margin-left:50px; font-family: arial;">
+                <a href="Start.php" style="color:white; text-decoration-line:none;">Christa</a>
+            </h1>
+        </div>
+        <div class="ondertitel">
+            <h1 style="color:white; margin-left:50px; font-family: arial; font-size:20px;">
+                Verwijderen
+            </h1>
+            <h1 style="display: flex; justify-content: flex-end; color:white; margin-left:50px; font-family: arial; font-size:20px; margin-top:-38px; padding-right:10px;">
+
+                <!--  <div style="margin-left:60%;"> -->
+                <button onclick="goBack()" style="border-radius:5px;
+                    width:100px; height: 30px;
+                     font-size: 16px;">Terug</button>
+                <button onclick="window.location.href = 'logout.php';"
+                        style="border-radius:5px;
+                    width:100px; height: 30px;
+                     font-size: 16px;">
+                    Log uit</button>
+                <!-- </div> -->
+            </h1>
+
+        </div>
+
+    </header>
+    </body>
+    </html>
+<br><br>
+    <div style="margin-left: 50px;">
+    <h1> Weet u zeker dat u de reservering van <?=$row['firstname']?> <?=$row['lastname']?> wilt verwijderen? </h1>
+
+    <form action="bevestigd_verwijderen.php" method="post">
+        <input type="hidden" name="reservering" value="<?=$id?>">
+        <input type="submit" value="Ja" style="border-radius:5px;
+                    width:100px; height: 30px;
+                     font-size: 16px;">
+    </form>
+    <button onclick="goBack()" style="border-radius:5px;
+                    width:100px; height: 30px;
+                     font-size: 16px;">Nee</button>
+    </div>
+    <script>
+        function goBack() {
+            window.history.back();
 
 
 
 
-<form action="bevestigd_verwijderen.php" method="post">
-    <input type="hidden" name="reservering" value="<?=$id?>">
-    <input type="submit" value="Ja">
-</form>
 
-<button onclick="goBack()">Nee</button>
+
+
+
+        }
+    </script>
 
 <?php } ?>
