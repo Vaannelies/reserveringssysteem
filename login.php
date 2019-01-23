1be2e3d52f5a0d73dc6fc8f9dcad0ca18b23d432
Annelies <43701941+Vaannelies@users.noreply.github.com>
2019-01-23 01:04:17 +0100

    opmaak beter en terugbutton werkend

--- login.php
+++ login.php
@@ -5,9 +5,6 @@
  * Date: 7-1-2019
  * Time: 15:59
  */
-
-
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -17,35 +14,58 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="stylesheet_home.css">
 </head>
 <body>
-
-
-
-
-
 <header>
-    <form action="loginprocess.php" method="post">
-        Gebruikersnaam:
-        <input type="text" name="username" required><br>
-        Wachtwoord:
-        <input type="password" name="password" required>
-        <button type="submit" name="login-submit">Log in</button>
-    </form>
+    <div class="titel">
+        <h1 style="color:white; margin-left:50px; font-family: arial;">
+            <a href="Start.php" style="color:white; text-decoration-line:none;">Christa</a>
+        </h1>
+    </div>
+    <div class="ondertitel">
+        <h1 style="color:white; margin-left:50px; font-family: arial; font-size:20px;">Inloggen
+        </h1>
+        <h1 style="display: flex; justify-content: flex-end; color:white; margin-left:50px; font-family: arial; font-size:20px; margin-top:-38px; padding-right:10px;">
 
+            <!--  <div style="margin-left:60%;"> -->
+            <button  onclick="goBack()"
+                     style="border-radius:5px;
+                    width:200px; height: 30px;
+                    font-size: 16px;">Terug</button>
 
-<!-- username is test, password is test -->
+            <!-- </div> -->
+        </h1>
+    </div>
+    <div style="margin-left:50px;">
+        <h1>Beheerders login</h1>
+    </div>
 </header>
 
+<div style="display:flex; justify-content: center;">
+<form action="loginprocess.php" method="post">
+    <div style="display:flex; flex-direction:column; justify-content:center; box-shadow:0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); padding:10px; margin-top:200px;">
+    <div id="usern"> Gebruikersnaam:
+    <input type="text" name="username" required><br>
+    </div>
+    <div id="passw" style="display:flex; justify-content:flex-end;margin-top:5px;">
+    Wachtwoord:
+    <input type="password" name="password" required>
+    </div>
 
-<main>
-
-</main>
-
-<form action="" method="post">
-
+    <button type="submit" name="login-submit" style="margin-top:5px;">Log in</button>
+    </div>
+    <!-- username is test, password is test -->
 </form>
+</div>
+
 </body>
+<script>
+    function goBack() {
+        window.history.back();
+
+    }
+</script>
 </html>
 
 
