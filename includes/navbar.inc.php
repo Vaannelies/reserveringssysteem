315d08ce6dd949ed2c96d40d493fae35312e30f7
Annelies <43701941+Vaannelies@users.noreply.github.com>
2019-02-03 14:34:38 +0100

    responsive

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -3,34 +3,37 @@
 ?>
 <!doctype html>
 <html lang="en">
+<head>
+    <link rel="stylesheet" href="stylesheet_navbar.css">
+</head>
 
 <body>
 
     <div class="titel" style="display:flex; justify-content:space-between;">
         <div id="titel">
-        <h1 style="color:white; margin-left:50px; font-family: arial;">
-            <a href="Start.php" style="color:white; text-decoration-line:none;">Christa</a>
-        </h1>
+            <h1 style="color:white; margin-left:50px; font-family: arial;">
+                <a href="Start.php" style="color:white; text-decoration-line:none;">Christa</a>
+            </h1>
         </div>
 
-        <div style="display:flex;">
+        <div class="menu" style="display:flex;"> <!-- menu -->
 
         <a class="menubutton" href="Start.php" style="color:white; text-decoration-line:none;">
-            <div id="menu" style=" text-align:center; width:150px; padding-top:5px;" >
+            <div id="menu" style=" text-align:center; padding-top:5px;" >
                 <h2>Home</h2>
             </div>
-
+        </a>
 
         <a class="menubutton" href="prijzen.php" style="color:white; text-decoration-line:none;">
-        <div id="menu" style=" text-align:center; width:150px; padding-top:5px;" >
-           <h2>Prijzen</h2>
-        </div>
+            <div id="menu" style=" text-align:center; padding-top:5px;" >
+               <h2>Prijzen</h2>
+            </div>
         </a>
 
         <a class="menubutton" href="aantal personen.php" style="color:white; text-decoration-line:none;">
-        <div id="menu" style=" text-align:center; width:150px; padding-top:5px; padding-right:25px; padding-left:25px;" >
-           <h2>Reserveren</h2>
-        </div>
+            <div id="menuright" style=" text-align:center; padding-top:5px;" >
+               <h2>Reserveren</h2>
+            </div>
         </a>
 
 
